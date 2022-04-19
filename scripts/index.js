c71a1d6157f573383b43b721a5adfc37f3596aee
Mena <sandym3000@hotmail.com>
2022-04-18 21:37:22 -0500

    Ordenamiento de burbuja

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,19 @@
 function sort() {
-   let value = document.getElementById("arr").value; 
-   let split = value.split(",");
-   let sort = split.sort(function (a, b) {
-       return a - b
-   } );
-   document.getElementById("valueInput").innerHTML = sort;
+  var value = document.getElementById("arr").value;
+  var split = value.split(",").map(n => Number.parseInt(n));
+  bubbleSort(split);
+  document.getElementById("valueInput").innerHTML = split;
+}
+
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let aux = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = aux;
+      }
+    }
+  }
+  return arr;
 }
